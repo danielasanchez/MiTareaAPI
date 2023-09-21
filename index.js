18fdb14b7e675978dcabbf17ac8b7fa29ae129fd
Daniela Sanchez <danielasanchez.itt@hotmail.com>
2023-09-21 15:15:40 -0700

    mi primer commit

---
+++ index.js
@@ -0,0 +1,73 @@
+function Leer() {
+    const peli = document.getElementById("input").value;
+    //obtain an apikey on this web
+    //http://www.omdbapi.com/apikey.aspx
+    const key='f0931e44';
+    const api_url=`http://www.omdbapi.com/?s=${peli}&apikey=${key}`
+    buscar1(api_url);
+}
+
+function buscar1(api_url){
+
+    fetch(api_url)
+      .then(data => {
+        return data.json()
+      }).then(resultado=>{
+            console.log(resultado);
+
+            const {Search=[]} = resultado;
+            
+            console.log(Search);
+            document.getElementById("lista").innerHTML='';
+
+            Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                         <img width='100%' src=${p.Poster} alt="No hay poster"></img>
+            </div>`;
+            })
+      });
+
+}
+
+const buscar2=async(api_url)=>{
+
+    const data= await fetch(api_url);
+    const respuesta= await data.json();
+    const Search = await respuesta.Search;
+
+    console.log(Search);
+
+    if(Search!=null)
+    {   
+        document.getElementById("lista").innerHTML='';
+        Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                    <img width='100%' src=${p.Poster} alt="No hay poster"></img></div>`;
+        })
+
+    }
+
+}    
+
+     
+const buscar3=async(api_url)=>{
+
+    const respuesta= await axios(api_url);
+    const Search = await respuesta.data.Search;
+    console.log(respuesta.data);
+    
+    console.log(Search);
+
+    
+    if(Search!=null)
+    {
+        document.getElementById("lista").innerHTML='';
+        
+        Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                    <img width='100%' src=${p.Poster} alt="No hay poster"></img></div>`;
+        })
+
+    }
+
+}    
